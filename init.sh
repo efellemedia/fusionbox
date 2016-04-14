edb35dad5ce24b5ca32c5a21b0fc349036d1d3b1
Shea Lewis <shea.lewis89@gmail.com>
2016-04-14 09:40:07 -0700

    Updated path

--- init.sh
+++ init.sh
@@ -4,6 +4,6 @@ fusionboxRoot=~/.fusionbox
 
 mkdir -p "$fusionboxRoot"
 
-cp -i src/stubs/Fusionbox.yaml "$fusionboxRoot/Fusionbox.yaml"
+cp -i "$fusionboxRoot/src/stubs/Fusionbox.yaml" "$fusionboxRoot/Fusionbox.yaml"
 
 echo "Fusionbox initialized! You will find your configuration file at $fusionboxRoot/Fusionbox.yaml"
