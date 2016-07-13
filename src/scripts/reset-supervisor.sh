17cd868eb2a2278be1c2d8c67cded55b01e4281c
Shea Lewis <shea.lewis89@gmail.com>
2016-07-13 13:14:45 -0700

    Ensure supervisor configurations are included

--- src/scripts/reset-supervisor.sh
+++ src/scripts/reset-supervisor.sh
@@ -1,3 +1,9 @@
 mkdir -p /etc/supervisor
 
+BLOCK="[include]
+files = supervisor/*.conf"
+
+FILE=/etc/supervisord.conf
+grep -q "$BLOCK" "$FILE" || echo "$BLOCK" >> "$FILE"
+
 rm -f /etc/supervisor/*
